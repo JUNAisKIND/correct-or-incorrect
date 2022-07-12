70ae5e4aea3fd262b62e7e217d3a659b12927e20
JUNAisKIND <0627juna@naver.com>
2022-07-12 22:43:00 +0900

    json

--- quiz.js
+++ quiz.js
@@ -2,6 +2,24 @@ fetch('https://junaiskind.github.io/correct-or-incorrect/sources.json')
   .then(Response => Response.text())
   .then(text => onLoad(JSON.parse(text)));
 
+
+const randomIndex = list => Math.trunc(Math.random() *list.length);
+
+function* nextQuiz(questions) {
+
+  while (questions.length > 0) {
+    const idx = randomIndex(questions);
+    yield questions.splice(idx, 1);
+  }
+
+  return false;
+
+}
+
+let lia;
 function onLoad(questions) {
-  questions
+  lia = questions;
+  [].slice()
+
+
 }
